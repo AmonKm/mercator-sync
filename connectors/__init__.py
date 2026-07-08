8856089516d8871e7ac39e29077f5699d0b5c9ed
Amon <166015459+AmonKm@users.noreply.github.com>
2026-07-08 16:31:00 +0200

    Add proxmox __init__.py

--- connectors/__init__.py
+++ connectors/__init__.py
@@ -1,8 +1,9 @@
 from .vcenter import VCenterConnector
 from .xoa     import XoaConnector
+from .proxmox import ProxmoxConnector
 
 REGISTRY: dict[str, type] = {
     "vcenter": VCenterConnector,
     "xoa":     XoaConnector,
+    "proxmox": ProxmoxConnector
 }
-# à commenter !
