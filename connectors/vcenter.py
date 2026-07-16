c41dfd401d0815867e5f7b798f11540e88fd96ce
Amon <166015459+AmonKm@users.noreply.github.com>
2026-07-16 10:56:02 +0200

    add disk temp vcenter.py

--- connectors/vcenter.py
+++ connectors/vcenter.py
@@ -34,7 +34,7 @@ class VCenterConnector(BaseConnector):
         requête.raise_for_status()
         return requête.json()
 
-    def fetch_vms(self, cluster_id: str) -> list[dict]: # On parcours l'ensemble des vms PAR cluster, même logique que xoa
+    def fetch_vms(self, cluster_id: str) -> list[dict]: # On parcours l'ensemble des vms PAR cluster
         requête = requests.get(
             f"{self.base_url}/api/vcenter/vm",
             params={"clusters": cluster_id},
@@ -44,28 +44,25 @@ class VCenterConnector(BaseConnector):
         return requête.json()
 
     def enrich_vm(self, vm_id: str, vm: dict) -> dict:
-        """Deux appels vCenter : détails VM + identité guest. Au besoin il est possible d'ajouter des appels pour enrichir les données..."""
+        """Deux appels vCenter : détails VM + identité guest."""
         r_details = requests.get(
             f"{self.base_url}/api/vcenter/vm/{vm_id}",
             headers=self.headers, verify=self.verify
         )
         r_details.raise_for_status()
         details = r_details.json()
-        # print(details)
         time.sleep(0.3)
 
         r_guest = requests.get(
             f"{self.base_url}/api/vcenter/vm/{vm_id}/guest/identity",
             headers=self.headers, verify=self.verify
         )
-        # guest/identity peut être indisponible (VM éteinte)
         guest = r_guest.json() if r_guest.status_code == 200 else {}
 
         return {
             **details,
             "guest": {
                 "ip_address": guest.get("ip_address", ""),
-                "full_name":  guest.get("full_name", {}),
             },
         }
     
@@ -73,23 +70,23 @@ class VCenterConnector(BaseConnector):
         cpu     = enriched.get("cpu", {}).get("count")
         mem_go  = round(enriched.get("memory", {}).get("size_MiB", 0) / 1024, 1)
         ip      = enriched.get("guest", {}).get("ip_address", "")
-        info    = enriched.get("guest", {}).get("full_name", {}).get("default_message", "")
-
+        disks_list = list(enriched.get("disks", {}).values())
         return {
             "name":             enriched.get("identity", {}).get("name", ""),
-            "description":      f"VM importée du Vcenter suivant : {self.base_url} ({vm_id})<br>{enriched.get('name_description', '')}", # Peut être mettre autre chose que url
-            "operating_system": info,
+            "description":      f"VM importée du Vcenter suivant : {self.name} ({vm_id})<br>{enriched.get('name_description', '')}",
+            "operating_system": enriched.get("guest_OS", ""),
             "address_ip":       ip,
             "cpu":              cpu or 0,
             "memory":           mem_go or 0,
             "attributes":       f"{self.config['name_id']}", # A modifier si l'on veut faire un ajout
-            "ext_refs": f"{{{self.name}}}{vm_id}", # utilisation de ext refs !
+            "ext_refs": f"{{{self.name}}}{vm_id}",
+            "disk": round(disks_list[0].get("capacity", 0) / 1024**3, 1) if disks_list else 0 # A changer, selon les cas !
         }
     
     def build_cluster_payload(self, cluster_id: str, cluster: dict) -> dict:
         return {
             "name":       cluster.get("name", ""),
-            "ext_refs": f"{{{self.name}}}{cluster_id}", 
+            "ext_refs": f"{{{self.name}}}{cluster_id}",
             "attributes":       f"{self.config['name_id']}",
             "type": "VMware",
             "description": f"Cluster provenant de la source : {self.name}"
