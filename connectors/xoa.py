4b64736dc3b23cf1b45ff30d78449b39b11d13ef
Amon <166015459+AmonKm@users.noreply.github.com>
2026-07-16 14:49:30 +0200

    +comment xoa.py

--- connectors/xoa.py
+++ connectors/xoa.py
@@ -1,17 +1,17 @@
 import os
 import requests
-from .base import BaseConnector
+from .base import BaseConnector 
 
 
-class XoaConnector(BaseConnector):
+class XoaConnector(BaseConnector): # Cette classe hérite directement de BaseConnector 
 
-    def authenticate(self) -> None:
+    def authenticate(self) -> None: # Méhode pour l'authentification. Créer un cookie avec le token et créer les variables selon l'instance. Ne renvoie rien.
         token = os.environ[self.config["auth"]["token_env"]]
         self.cookies = {"authenticationToken": token}
         self.verify  = self.config.get("verify_ssl", True)
         self.base_url = os.environ[self.config["base_url"]]
 
-    def fetch_clusters(self) -> list[dict]:
+    def fetch_clusters(self) -> list[dict]: # Permet d'aller chercher un cluster souhaité. Renvoie une liste de dictionnaire.
         pool_id = self.config["pool_id"]
 
         requête = requests.get(
@@ -26,7 +26,7 @@ class XoaConnector(BaseConnector):
 
         return [pool]
 
-    def fetch_vms(self, cluster_id: str) -> list[dict]: # Plusieurs appels si plusieurs clusters... A voir pour prendre une liste d'id de cluster ?
+    def fetch_vms(self, cluster_id: str) -> list[dict]: # Prend en argument l'id d'un cluster pour parcourir les VMs de ce cluster. Renvoie une liste de dictionnaire.
        requête= requests.get(
             f"{self.base_url}/rest/v0/vms",
             params={"filter": f"$pool:{cluster_id}", "fields": "id,name_label,power_state"},
@@ -35,7 +35,7 @@ class XoaConnector(BaseConnector):
        requête.raise_for_status()
        return requête.json()
 
-    def enrich_vm(self, vm_id: str, _vm: dict) -> dict:
+    def enrich_vm(self, vm_id: str, _vm: dict) -> dict: # Prend en argument l'id d'une VM et renvoie le dictionnaire associé avec les données de la VM.
         requête= requests.get(
             f"{self.base_url}/rest/v0/vms/{vm_id}",
             cookies=self.cookies, verify=self.verify, timeout=10
@@ -43,7 +43,7 @@ class XoaConnector(BaseConnector):
         requête.raise_for_status()
         return requête.json()
 
-    def build_vm_payload(self, vm_id: str, enriched: dict) -> dict:
+    def build_vm_payload(self, vm_id: str, enriched: dict) -> dict: # Prend en argument l'id d'une vm et le dictionnaire d'infos d'une VM. Renvoie un dictionnaire adapté à Mercator.
         cpu     = enriched.get("CPUs", {}).get("number")
         mem_go  = round(enriched.get("memory", {}).get("size", 0) / (1024 ** 3), 1)
         os_info = enriched.get("os_version") or {}
@@ -67,7 +67,7 @@ class XoaConnector(BaseConnector):
             "ext_refs": f"{{{self.name}}}{vm_id}",
         }
     
-    def build_cluster_payload(self, cluster_id: str, cluster: dict) -> dict:
+    def build_cluster_payload(self, cluster_id: str, cluster: dict) -> dict: # Prend en argument l'id d'un cluster + le dictionnaire d'infos d'un cluster. Renvoie un dictionnaire adapté à Mercator.
         return {
             "name":       cluster.get("name_label", cluster_id)[:32],
             "ext_refs":   f"{{{self.name}}}{cluster_id}",
